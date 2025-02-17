50f0400a461ee92f0bb3c4a999b500318b7cf833
Trần Tuấn Thành <152388776+thanhtr155@users.noreply.github.com>
2025-02-17 09:44:17 +0700

    them file choi voi AI (test)

--- Tic-Tac-Toe frontend/tic-tac-toe/src/App.js
+++ Tic-Tac-Toe frontend/tic-tac-toe/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage';
 import Play from './pages/Play';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import PlayWithAI from './pages/PlayWithAI';
 import PlayOnline from './pages/PlayOnline';
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
     <Route path='/' element={<HomePage />}/>
     <Route path='/login' element={<Login />}/>
     <Route path='/register' element={<Register />}/>
-    <Route path="/playai" element={<Play />}/>
+    <Route path="/play" element={<Play />}/>
+    <Route path="/playai" element={<PlayWithAI />}/>
     <Route path="/playonline" element={<PlayOnline />}/>
 
    </Routes>
